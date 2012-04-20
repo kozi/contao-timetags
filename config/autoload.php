61a6134f4f244ef5dc700454b8ee50f0ca39c779
kozi <martin@kozianka-online.de>
2012-04-20 08:54:59 +0200

    Updates for Contao 3

--- config/autoload.php
+++ config/autoload.php
@@ -1,30 +1,36 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
 
 /**
- * Contao webCMS
- * Copyright (C) 2011 Leo Feyer
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
- * 
+ * version 3 of the License, or (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
- * PHP version 5
- * @copyright  Martin Kozianka 2011-2012
- * @author     Martin Kozianka <martin@kozianka-online.de>
- * @package    timetags 
+ * PHP version 5.3
+ * @copyright  Martin Kozianka 2012
+ * @author     Martin Kozianka <http://kozianka-online.de>
+ * @package    themelinks
  * @license    LGPL
  */
- 
-$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('Timetags', 'replaceTags');
 
-?>
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array(
+	'Timetags'    => 'system/modules/timetags/Timetags.php'
+));
