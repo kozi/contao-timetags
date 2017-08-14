08f6f6998ca6a17af84ff8e49e1588fb838588e0
Martin Kozianka <kozianka@otris.de>
2017-08-14 12:44:51 +0200

    Added first version for contao 3.5.x and 4.4.x

--- contao/classes/Timetags.php
+++ contao/classes/Timetags.php
@@ -2,11 +2,11 @@
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2014 Leo Feyer
+ * Copyright (C) 2005-2017 Leo Feyer
  *
  *
  * PHP version 5
- * @copyright  Martin Kozianka 2011-2014 <http://kozianka.de/>
+ * @copyright  Martin Kozianka 2011-2017
  * @author     Martin Kozianka <http://kozianka.de/>
  * @package    timetags
  * @license    LGPL 
@@ -17,7 +17,7 @@
 /**
  * Class Timetags
  *
- * @copyright  Martin Kozianka 2011-2014
+ * @copyright  Martin Kozianka 2011-2017
  * @author     Martin Kozianka <http://kozianka.de>
  * @package    timetags
  */
@@ -199,7 +199,7 @@ class Timetags extends Frontend {
 	private function relativeTime(){
 	    $difference = time() - $this->date->timestamp;
 
-		$lengths = array("60","60","24","7","4.35","12","10");
+		$lengths = ["60","60","24","7","4.35","12","10"];
 
 	    for($j = 0; $difference >= $lengths[$j] && $j < sizeof($lengths); $j++) {
 	    	$difference /= $lengths[$j];
--- contao/languages/de/default.php
+++ contao/languages/de/default.php
@@ -2,11 +2,11 @@
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2014 Leo Feyer
+ * Copyright (C) 2005-2017 Leo Feyer
  *
  *
  * PHP version 5
- * @copyright  Martin Kozianka 2011-2014 <http://kozianka.de/>
+ * @copyright  Martin Kozianka 2011-2017
  * @author     Martin Kozianka <http://kozianka.de/>
  * @package    timetags
  * @license    LGPL 
@@ -15,8 +15,8 @@
 
 
 $GLOBALS['TL_LANG']['FMD']['timetags_timesince_format']     = "[vor %s]:";
-$GLOBALS['TL_LANG']['FMD']['timetags_timesince']            = array("Sekunde", "Minute", "Stunde", "Tag", "Woche", "Monat", "Jahr", "Jahrzehnt");
-$GLOBALS['TL_LANG']['FMD']['timetags_timesince_plural']     = array("Sekunden", "Minuten", "Stunden", "Tagen", "Wochen", "Monaten", "Jahren", "Jahrzehnten");
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince']            = ["Sekunde", "Minute", "Stunde", "Tag", "Woche", "Monat", "Jahr", "Jahrzehnt"];
+$GLOBALS['TL_LANG']['FMD']['timetags_timesince_plural']     = ["Sekunden", "Minuten", "Stunden", "Tagen", "Wochen", "Monaten", "Jahren", "Jahrzehnten"];
 
-$GLOBALS['TL_LANG']['FMD']['timetags_countdown_plural']     = array("Sekunden", "Minuten", "Stunden", "Tage");
-$GLOBALS['TL_LANG']['FMD']['timetags_countdown']            = array("Sekunde", "Minute", "Stunde", "Tag");
+$GLOBALS['TL_LANG']['FMD']['timetags_countdown_plural']     = ["Sekunden", "Minuten", "Stunden", "Tage"];
+$GLOBALS['TL_LANG']['FMD']['timetags_countdown']            = ["Sekunde", "Minute", "Stunde", "Tag"];
